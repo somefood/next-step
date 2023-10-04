e3967e8f454812d2c70adf1015deaad0ca9471a4
Seokju Hong <iamseokjuhong@gmail.com>
2023-10-05 01:00:57 +0900

    5장 - HttpResponse 작성 중

---
+++ web-application-server-master/src/main/java/http/HttpResponse.java
@@ -0,0 +1,21 @@
+package http;
+
+import java.io.OutputStream;
+
+public class HttpResponse {
+    public HttpResponse(OutputStream os) {
+        
+    }
+
+    public void forward(String path) {
+
+    }
+
+    public void sendRedirect(String path) {
+
+    }
+
+    public void addHeader(String headerKey, String headerValue) {
+
+    }
+}
