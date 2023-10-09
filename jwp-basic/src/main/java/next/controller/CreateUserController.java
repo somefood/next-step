0379a03913eb0da9e265faa43caa08f7f375ea3d
Seokju Hong <iamseokjuhong@gmail.com>
2023-10-10 00:37:11 +0900

    6.5 마무리

--- jwp-basic/src/main/java/next/controller/CreateUserController.java
+++ jwp-basic/src/main/java/next/controller/CreateUserController.java
@@ -1,39 +1,32 @@
 package next.controller;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import core.db.DataBase;
+import core.mvc.Controller;
+import next.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import core.db.DataBase;
-import next.model.User;
+public class CreateUserController implements Controller {
 
-@WebServlet(value = { "/users/create", "/users/form" })
-public class CreateUserController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-    private static final Logger log = LoggerFactory.getLogger(CreateUserController.class);
+    private static final Logger logger = LoggerFactory.getLogger(CreateUserController.class);
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher rd = req.getRequestDispatcher("/user/form.jsp");
-        rd.forward(req, resp);
-    }
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        logger.info("회원가입 시도");
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
-                req.getParameter("email"));
-        log.debug("User : {}", user);
+        User user = new User(
+                request.getParameter("userId"),
+                request.getParameter("password"),
+                request.getParameter("name"),
+                request.getParameter("email")
+        );
+        logger.debug("User : {}", user);
 
         DataBase.addUser(user);
 
-        resp.sendRedirect("/");
+        return "redirect:/";
     }
 }
