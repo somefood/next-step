7353139a899765ca45589c0da5a275bfe8c4ac7d
somefood <iamseokjuhong@gmail.com>
2023-10-11 12:00:58 +0900

    7.1.3까지

--- jwp-basic-chpater7/src/main/java/next/controller/ListUserController.java
+++ jwp-basic-chpater7/src/main/java/next/controller/ListUserController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import core.db.DataBase;
 import core.mvc.Controller;
+import next.dao.UserDao;
 
 public class ListUserController implements Controller {
     @Override
@@ -13,7 +14,8 @@ public class ListUserController implements Controller {
             return "redirect:/users/loginForm";
         }
 
-        req.setAttribute("users", DataBase.findAll());
+        UserDao userDao = new UserDao();
+        req.setAttribute("users", userDao.findAll());
         return "/user/list.jsp";
     }
 }
