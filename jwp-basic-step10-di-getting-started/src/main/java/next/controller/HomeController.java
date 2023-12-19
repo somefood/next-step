c3146f95e1fe54c8fb05f45d1e074c8588bafaba
Seokju Hong <iamseokjuhong@gmail.com>
2023-12-20 00:00:35 +0900

    10장 시작

---
+++ jwp-basic-step10-di-getting-started/src/main/java/next/controller/HomeController.java
@@ -0,0 +1,20 @@
+package next.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import core.mvc.AbstractController;
+import core.mvc.ModelAndView;
+import next.dao.QuestionDao;
+
+public class HomeController extends AbstractController {
+    private QuestionDao questionDao;
+
+    public HomeController(QuestionDao questionDao) {
+        this.questionDao = questionDao;
+    }
+
+    public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return jspView("home.jsp").addObject("questions", questionDao.findAll());
+    }
+}
---
+++ jwp-basic-step10-di-getting-started/src/main/java/next/service/QnaService.java
@@ -0,0 +1,40 @@
+package next.service;
+
+import java.util.List;
+
+import next.CannotDeleteException;
+import next.dao.AnswerDao;
+import next.dao.QuestionDao;
+import next.model.Answer;
+import next.model.Question;
+import next.model.User;
+
+public class QnaService {
+    private QuestionDao questionDao;
+    private AnswerDao answerDao;
+
+    public QnaService(QuestionDao questionDao, AnswerDao answerDao) {
+        this.questionDao = questionDao;
+        this.answerDao = answerDao;
+    }
+
+    public Question findById(long questionId) {
+        return questionDao.findById(questionId);
+    }
+
+    public List<Answer> findAllByQuestionId(long questionId) {
+        return answerDao.findAllByQuestionId(questionId);
+    }
+
+    public void deleteQuestion(long questionId, User user) throws CannotDeleteException {
+        Question question = questionDao.findById(questionId);
+        if (question == null) {
+            throw new CannotDeleteException("존재하지 않는 질문입니다.");
+        }
+
+        List<Answer> answers = answerDao.findAllByQuestionId(questionId);
+        if (question.canDelete(user, answers)) {
+            questionDao.delete(questionId);
+        }
+    }
+}
